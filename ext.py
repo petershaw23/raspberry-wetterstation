d96ed70653b16de14481a4173f972cc2336ff913
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2018-12-09 13:00:08 +0100

    Add files via upload

--- ext.py
+++ ext.py
@@ -1,93 +1,84 @@
-#!/usr/bin/env python
-import httplib, urllib, thingspeak, time
+#!/usr/bin/env python3
 
-# lib fuer DHT22 sensor luftfeuchtigkeit
-import Adafruit_DHT
+import http.client, urllib.parse
+import thingspeak, Adafruit_DHT
+import board
+import busio
+import adafruit_bmp280
+import time
 
-# lib fuer bmp280 sensor luftdruck (laed adafruit "CircuitPython" und "bmp280" libraries)
-import board, busio, adafruit_bmp280
+key = 'UUIR9GYRF0SQ2SQU'
 
-# lib fuer taupunktberechnung:
+# fuer taupunkt:
+A = 17.27
+B = 237.7
 import math
 
 
-
-
-#konstanten fuer taupunktberechnung
-A = 17.27
-B = 237.7
-
-# festlegungen fuer dht22:
+# fuer dht22:
 pinDHT = 27
 sensorDHT = Adafruit_DHT.DHT22
 
-# festlegungen fuer BMP280
+#fuer BMP 280 luftdrucksensor
 i2c = busio.I2C(board.SCL, board.SDA)
-sensorBMP = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-sensorBMP.sea_level_pressure = 1016.5
-
-#channel write key fuer thingspeak
-key = '83N9AKFZ0QUCMO7M'
-
-#intervall des scripts
-sleep = 120
+bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+#change this to match the location's pressure (hPa) at sea level
+bmp280.sea_level_pressure = 1013.25
 
 def ext():
-   while True:
-       	
-	#Kabelsensor 1
- 	tempfileex1 = open("/sys/bus/w1/devices/28-02131dc072aa/w1_slave")
-        thetext01 = tempfileex1.read()
-        tempfileex1.close()
-        tempdataex1 = thetext01.split("\n")[1].split(" ")[9]
-        temperatureex1 = float(tempdataex1[2:])
-        temperatureex1 = temperatureex1 / 1000
-        kabel1 = temperatureex1
+    #Kabel Temp Sensor 1
+    tempfileex1 = open("/sys/bus/w1/devices/28-02131dc072aa/w1_slave")
+    thetext01 = tempfileex1.read()
+    tempfileex1.close()
+    tempdataex1 = thetext01.split("\n")[1].split(" ")[9]
+    temperatureex1 = float(tempdataex1[2:])
+    temperatureex1 = temperatureex1 / 1000
+    #temperatureex1 = 20 falls wackelkontakt
+    tempex1 = temperatureex1
 
-	#Kabelsensor 2
- 	tempfileex2 = open("/sys/bus/w1/devices/28-011432f02f58/w1_slave")
-        thetext02 = tempfileex2.read()
-        tempfileex2.close()
-        tempdataex2 = thetext02.split("\n")[1].split(" ")[9]
-        temperatureex2 = float(tempdataex2[2:])
-        temperatureex2 = temperatureex2 / 1000
-        kabel2 = temperatureex2
+    #Kabel Temp Sensor 2
+    tempfileex2 = open("/sys/bus/w1/devices/28-011432f02f58/w1_slave")
+    thetext02 = tempfileex2.read()
+    tempfileex2.close()
+    tempdataex2 = thetext02.split("\n")[1].split(" ")[9]
+    temperatureex2 = float(tempdataex2[2:])
+    temperatureex2 = temperatureex2 / 1000
+    tempex2 = temperatureex2
 
-	#Temperature DHT und Luftfeuchtigkeit DHT
-	humidityDHT, temperatureDHT = Adafruit_DHT.read_retry(sensorDHT, pinDHT)
+    #bmp280.temperature
+    bmptemp = bmp280.temperature
 	
-	#taupunktberechnung
-	alpha = ((A * temperatureDHT) / (B + temperatureDHT)) + math.log(humidityDHT/100.0)
-	taupunkt = (B * alpha) / (A - alpha)
+	#DHT22 script
+    humidity22, temperature22 = Adafruit_DHT.read_retry(sensorDHT, pinDHT)
+        
+    #taupunkt
+    alpha = ((A * temperature22) / (B + temperature22)) + math.log(humidity22/100.0)
+    taupunkt = (B * alpha) / (A - alpha)
 
-	#luftdruck, temperatur, hoehe BMP
-	luftdruck = sensorBMP.pressure
-	temperatureBMP = sensorBMP.temperature
-	hoehe = sensorBMP.altitude
+    #luftdruck
+    luftdruck = bmp280.pressure
+    
+	
+	
 
-        params = urllib.urlencode({'field1': kabel1, 'field2': kabel2, 'field3': temperatureDHT, 'field4': temperatureBMP, 'field5': humidityDHT, 'field6': taupunkt, 'field7': luftdruck, 'field8': hoehe, 'key':key })
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection("api.thingspeak.com:80")
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
+    params = urllib.parse.urlencode({'field1': tempex1, 'field2': tempex2, 'field3': bmptemp, 'field4': temperature22, 'field5': humidity22, 'field6': taupunkt, 'field7': luftdruck, 'key':key })
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = http.client.HTTPConnection("api.thingspeak.com:80")
+    
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
             
-	    print 'kabel1 = 		{0:0.2f} deg C'.format(kabel1)
-	    print 'kabel2 = 		{0:0.2f} deg C'.format(kabel2)
-	    print 'temperatureDHT = {0:0.2f} deg C'.format(temperatureDHT)	    
-	    print 'temperatureBMP = {0:0.2f} deg C'.format(temperatureBMP)
-	    print 'humidityDHT = 	{0:0.2f} % rel'.format(humidityDHT)
-	    print 'taupunkt = 		{0:0.2f} deg C'.format(taupunkt)
-	    print 'luftdruck = 		{0:0.2f} Luftfeuchtigkeit %'.format(luftdruck)
-		print 'hoehe = 			{0:0.2f} m ueber Meeresspiegel'.format(hoehe)
-            print response.status, response.reason
-            data = response.read()
-            conn.close()
-        except:
-            print "connection failed"
-        break
-#sleep for desired amount of time
-if __name__ == "__main__":
-        while True:
-                ext()
-                time.sleep(sleep)
+    print (tempex1)
+    print (tempex2)	    
+    print (bmptemp)
+    print (temperature22)
+    print (humidity22)
+    print (taupunkt)
+    print (luftdruck)
+    print (response.status, response.reason)
+    data = response.read()
+    conn.close()
+
+    
+ext()
+            
--- int.py
+++ int.py
@@ -1,58 +1,54 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import httplib, urllib, thingspeak
+import http.client, urllib.parse
+import thingspeak, Adafruit_DHT
+#import digitalio, busio, adafruit_bmp280
 import time
 
 
-sleep = 120
 key = 'OL32V7QUCKN8Y4NU'
 
+
 def int():
-   while True:
+    #extract CPU temperature from pi control csv file CORETEMP
+    inputfile1 = "/var/www/html/resources/log/statistic/coretemp.csv"
+    with open(inputfile1, "r") as f:
+        for line in f: pass
+        temp = line[11:]
+
+    #extract freq
+    inputfile3 = "/var/www/html/resources/log/statistic/cpufrequency.csv"
+    with open(inputfile3, "r") as h:
+        for line in h: pass
+        freq = line[11:]
+
+    #extract cpuload
+    inputfile4 = "/var/www/html/resources/log/statistic/cpuload.csv"
+    with open(inputfile4, "r") as i:
+        for line in i: pass
+        cpuload = line[11:]
+
+    #extract ram
+    inputfile5 = "/var/www/html/resources/log/statistic/ram.csv"
+    with open(inputfile5, "r") as j:
+        for line in j: pass
+        ram = line[11:]
+
+    params = urllib.parse.urlencode({'field1': temp, 'field2': freq, 'field3': cpuload, 'field4': ram, 'key':key })
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = http.client.HTTPConnection("api.thingspeak.com:80")
     
-	#extract CPU temperature from pi control csv file CORETEMP
-	inputfile1 = "/var/www/html/resources/log/statistic/coretemp.csv"
-	with open(inputfile1, "r") as f:
-		for line in f: pass
-		CPUtemp = line[11:]
-
-	#extract freq
-	inputfile3 = "/var/www/html/resources/log/statistic/cpufrequency.csv"
-	with open(inputfile3, "r") as h:
-		for line in h: pass
-		CPUfreq = line[11:]
-
-	#extract cpuload
-	inputfile4 = "/var/www/html/resources/log/statistic/cpuload.csv"
-	with open(inputfile4, "r") as i:
-		for line in i: pass
-		CPUload = line[11:]
-
-	#extract ram
-	inputfile5 = "/var/www/html/resources/log/statistic/ram.csv"
-	with open(inputfile5, "r") as j:
-		for line in j: pass
-		ram = line[11:]
-
-        params = urllib.urlencode({'field1': CPUtemp, 'field2': CPUfreq, 'field3': CPUload, 'field4': ram, 'key':key })
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection("api.thingspeak.com:80")
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
             
-	    print 'CPUtemp = {0:0.2f} deg C'.format(temp)    
-	    print 'freq = {0:0.2f} mhz'.format(freq)
-	    print 'cpuload = {0:0.2f} % CPU'.format(cpuload)
-	    print 'ram = {0:0.2f} % RAM used'.format(ram)
-            print response.status, response.reason
-            data = response.read()
-            conn.close()
-        except:
-            print "connection failed"
-        break
-#sleep for desired amount of time
-if __name__ == "__main__":
-        while True:
-                int()
-                time.sleep(sleep)
+    print (temp)
+    print (freq)
+    print (cpuload)
+    print (ram)
+    print (response.status, response.reason)
+    data = response.read()
+    conn.close()
+
+    
+int()
+            

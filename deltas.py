3ac3c84d54b099b0bb58aed3e48ef1cb9c3b60e1
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 16:52:58 +0200

    Update deltas.py

--- deltas.py
+++ deltas.py
@@ -1,44 +1,38 @@
-import time
+!/usr/bin/python3
+import json
+import requests
 import http.client, urllib.parse
-import thingspeak
-time.sleep(45)
-try:
-    chPi1 = thingspeak.Channel(647418)
-    outRAWPi1 = chPi1.get({'results':1})
-    outSplitPi1 = outRAWPi1.split('\"')
-    outTempPi1 = outSplitPi1[-18]
-    outHumiPi1 = outSplitPi1[-14]
-
-    chD1 = thingspeak.Channel(843073)
-    outRAWD1 = chD1.get({'results':1})
-    outSplitD1 = outRAWD1.split('\"')
-    outTempD1 = outSplitD1[-14]
-    outHumiD1 = outSplitD1[-10]
+# pi1 data
+data1 = requests.get(url="https://api.thingspeak.com/channels/647418/feeds.json?results=1")
+jsonobj1 = json.loads(data1.content.decode('utf-8'))
+tempPi1 = (jsonobj1["feeds"][0]["field3"])
+humiPi1 = (jsonobj1["feeds"][0]["field5"])
 
 
-except: #falls offline
-    outTempPi1 = '1'
-    outHumiPi1 = '1'
-    outTempD1 = '1'
-    outHumiD1 = '1'
+# d1 mini data
+data2 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
+jsonobj2 = json.loads(data2.content.decode('utf-8'))
+tempD1 = (jsonobj2["feeds"][0]["field1"])
+humiD1 = (jsonobj2["feeds"][0]["field2"])
 
 
-deltaT = round(float(outTempPi1) - float(outTempD1), 1)
-deltaH = round(float(outHumiPi1) - float(outHumiD1), 1)
 
-print ('Pi1: temp '+str(outTempPi1)+'  humidity: '+str(outHumiPi1))
-print ('D1: temp '+str(outTempD1)+'  humidity: '+str(outHumiD1))
+deltaT = round(float(tempPi1) - float(tempD1), 3)
+deltaH = round(float(humiPi1) - float(humiD1), 3)
+
+print ('Pi1: temp '+str(tempPi1)+'  humidity: '+str(humiPi1))
+print ('D1: temp '+str(tempD1)+'  humidity: '+str(humiD1))
 print ('Delta T: '+str(deltaT))
 print ('Delta H: '+str(deltaH))
-#if deltaT > 1:
-#    print ('Lüften! Draussen ist es kälter!')
-#if deltaH > 2:
-#    print ('Lüften! Draussen ist es trockener!')
+if deltaT > 1:
+    print ('Lüften! Draussen ist es kälter!')
+if deltaH > 2:
+    print ('Lüften! Draussen ist es trockener!')
 
 
 #write deltas to thingspeak channel
 
-key = 'XXXXXXXXXXX' #dein thingspeak write key
+key = 'KGXTB1D2NUPP4G5T' #dein thingspeak write key
 
 #write to thingspeak via urllib
 params = urllib.parse.urlencode({'field4': deltaT, 'field5': deltaH, 'key':key })
@@ -50,5 +44,3 @@ print (response.status, response.reason)
    
 data = response.read()
 conn.close()
-
-

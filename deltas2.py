07690d56c365854aac33c079e9fd07276814d218
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 16:36:51 +0200

    Update deltas2.py

--- deltas2.py
+++ deltas2.py
@@ -4,15 +4,43 @@ import requests
 
 # pi1 data
 data1 = requests.get(url="https://api.thingspeak.com/channels/647418/feeds.json?results=1")
-jsonobj1 = json.loads(data1.content)
-#print (jsonobj1)
+jsonobj1 = json.loads(data1.content.decode('utf-8'))
+tempPi1 = (jsonobj1["feeds"][0]["field3"])
+humiPi1 = (jsonobj1["feeds"][0]["field5"])
 
 
 
 # d1 mini data
 data2 = requests.get(url="https://api.thingspeak.com/channels/647418/feeds.json?results=1")
-jsonobj2 = json.loads(data2.content)
-#print (jsonobj2)
+jsonobj2 = json.loads(data2.content.decode('utf-8'))
+tempD1 = (jsonobj1["feeds"][0]["field1"])
+humiD1 = (jsonobj1["feeds"][0]["field2"])
 
 
-#depart = (jsonobj["routes"][0]["legs"][0]["arrival_time"]["text"])
+deltaT = round(float(tempPi1) - float(tempD1), 1)
+deltaH = round(float(humiPi1) - float(humiD1), 1)
+
+print ('Pi1: temp '+str(tempPi1)+'  humidity: '+str(humiPi1))
+print ('D1: temp '+str(tempD1)+'  humidity: '+str(humiD1))
+print ('Delta T: '+str(deltaT))
+print ('Delta H: '+str(deltaH))
+#if deltaT > 1:
+#    print ('Lüften! Draussen ist es kälter!')
+#if deltaH > 2:
+#    print ('Lüften! Draussen ist es trockener!')
+
+
+#write deltas to thingspeak channel
+
+key = 'XXXXXXXXXXX' #dein thingspeak write key
+
+#write to thingspeak via urllib
+params = urllib.parse.urlencode({'field4': deltaT, 'field5': deltaH, 'key':key })
+headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+conn = http.client.HTTPConnection("api.thingspeak.com:80")
+conn.request("POST", "/update", params, headers)
+response = conn.getresponse()
+print (response.status, response.reason)
+   
+data = response.read()
+conn.close()

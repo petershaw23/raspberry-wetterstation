e9eb4b1462496965b54284fa4ecec8b9daf00d02
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-15 09:06:05 +0200

    Update ext2.py

--- ext2.py
+++ ext2.py
@@ -2,7 +2,7 @@
 import time
 time.sleep(20) #kann geloescht werden, nur bei benutzung mit crontab @reboot machts sinn
 import http.client, urllib.parse
-#import thingspeak
+#import thingspeak #old method
 import Adafruit_DHT
 import board
 import busio
@@ -27,6 +27,10 @@ bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 #change this to match the location's pressure (hPa) at sea level
 bmp280.sea_level_pressure = 1013.25
 
+
+
+
+
 def ext():
     #bmp280 script
     bmptempRaw = bmp280.temperature
@@ -48,9 +52,20 @@ def ext():
     alpha = ((A * mittelRaw) / (B + mittelRaw)) + math.log(humidity22Raw/100.0)
     taupunktRaw = (B * alpha) / (A - alpha)
     taupunkt = "%.1f" % taupunktRaw
+    # d1 mini data
+    data2 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
+    jsonobj2 = json.loads(data2.content.decode('utf-8'))
+    tempD1 = (jsonobj2["feeds"][0]["field1"])
+    humiD1 = (jsonobj2["feeds"][0]["field2"])
 
+
+
+    deltaT = round(float(bmptemp) - float(tempD1), 3)
+    deltaH = round(float(humidity22) - float(humiD1), 3)
+
+    
     #write to thingspeak via urllib
-    params = urllib.parse.urlencode({'field3': bmptemp, 'field4': temperature22, 'field5': humidity22, 'field6': taupunkt, 'field7': luftdruck, 'field8': mittel, 'key':key })
+    params = urllib.parse.urlencode({'field1': deltaT, 'field2': deltaH, 'field3': bmptemp, 'field4': temperature22, 'field5': humidity22, 'field6': taupunkt, 'field7': luftdruck, 'field8': mittel, 'key':key })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
     conn.request("POST", "/update", params, headers)

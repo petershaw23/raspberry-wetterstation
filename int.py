e85359569977bc87a2e3f3128b38ff233dd5928c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2021-04-13 17:26:46 +0200

    Update int.py

--- int.py
+++ int.py
@@ -26,13 +26,28 @@ def int():
     with open("/proc/net/dev") as f:
         content = f.readlines()
     content = [x.strip() for x in content] 
-    #print (content[3]) #check if correnct device, should read "eth0 ..."
+    #print (content[3]) #check if correct device, should read "eth0 ..."
     lan = str(content[3]).split()
     rxraw = int(lan[1])
     #print (rxraw)
     rxMB = round((rxraw / 1000 / 1000),4)
     print (rxMB)
-    params = urllib.parse.urlencode({'field1': temp, 'field2': freq, 'field3': cpuload, 'field4': ram, 'field5': rxMB,'key':key })
+    # data from thingspeak
+    data = requests.get(url="https://api.thingspeak.com/channels/646236/feeds.json?results=1")
+    jsonobj = json.loads(data.content.decode('utf-8'))
+    rxMB_old = float(jsonobj["feeds"][0]["field5"])
+    print ('rxMB_old: ' +str(rxMB_old))
+    last_entry = jsonobj["feeds"][0]["created_at"] #time of last entry
+    print ('age of rxMB_old: ' +str(last_entry))
+    # calculate deltaT and deltaH
+    deltaRX = float(rxMB) - float(rxMB_old)
+    print ('deltaRX: ' +str(deltaRX))
+    # calculate kb/sec average last 5 minutes
+    
+    
+  
+    # now write new values to thingspeak
+    params = urllib.parse.urlencode({'field1': temp, 'field2': freq, 'field3': cpuload, 'field4': ram, 'field5': rxMB, 'field6': deltaRX, 'key':key })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
     conn.request("POST", "/update", params, headers)
